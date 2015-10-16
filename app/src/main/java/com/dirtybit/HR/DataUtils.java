bc5684d7ea375e694e93d50db6c1cabfb8a6a0da
RituBansal <ritubansalmail@gmail.com>
2015-10-16 22:39:39 +0530

    DataUtil

---
+++ app/src/main/java/com/dirtybit/HR/DataUtils.java
@@ -0,0 +1,16 @@
+package com.dirtybit.HR;
+
+/**
+ * Created by MMT5505 on 16-10-2015.
+ */
+public class DataUtils {
+
+    final static int POLICY_TIME = 0;
+    final static int POLICY_DRESS = 1;
+    final static int POLICY_LEAVE = 2;
+    final static int POLICY_TRAVEL = 3;
+    final static int POLICY_FOOD = 4;
+    final static int POLICY_COMPOFF = 5;
+
+
+}
