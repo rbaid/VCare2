1ef4be80b0b189ad39456cb1de0db7d080438d31
rishabh <rishabhbaid1@gmail.com>
2015-10-17 01:23:08 +0530

    Employee Scree UI Update

--- app/src/main/java/com/dirtybit/vcare/EmployeeMainActivity.java
+++ app/src/main/java/com/dirtybit/vcare/EmployeeMainActivity.java
@@ -1,11 +1,15 @@
 package com.dirtybit.vcare;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class EmployeeMainActivity extends AppCompatActivity {
 
@@ -20,10 +24,63 @@ public class EmployeeMainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                findViewById(R.id.event).setVisibility(View.GONE);
+                findViewById(R.id.policy).setVisibility(View.GONE);
+                findViewById(R.id.general).setVisibility(View.VISIBLE);
             }
         });
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.general).setVisibility(View.GONE);
+                findViewById(R.id.policy).setVisibility(View.GONE);
+                findViewById(R.id.event).setVisibility(View.VISIBLE);
+                Spinner spinner = (Spinner) findViewById(R.id.event_spinner);
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(EmployeeMainActivity.this,
+                        R.array.event_array, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        adapterView.getItemAtPosition(i);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+            }
+        });
+
+        FloatingActionButton fa3 = (FloatingActionButton) findViewById(R.id.fab3);
+        fa3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.event).setVisibility(View.GONE);
+                findViewById(R.id.general).setVisibility(View.GONE);
+                findViewById(R.id.policy).setVisibility(View.VISIBLE);
+                Spinner spinner = (Spinner) findViewById(R.id.policy_spinner);
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(EmployeeMainActivity.this,
+                        R.array.policy_array, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        adapterView.getItemAtPosition(i);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+            }
+        });
+
     }
 
 }
